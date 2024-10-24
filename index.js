e3cfd8f1088f25c28c144072130a53e53a3e8d72
luisgarciasv <luisgarciasv3@gmail.com>
2024-10-24 09:26:36 -0600

    refactor: change args-parser, now it only formats arguments passed to the app

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import  { argv } from 'node:process'
-import  fs  from 'node:fs/promises'
-import { argsParser } from './lib/args-parser.js'
-import { error } from 'node:console'
+import { argv } from 'node:process';
+import { argsParser } from './lib/args-parser.js';
+import { createJSON } from './lib/create-json.js';
 
-const args = argv.slice(2) 
+const args = argv.slice(2);
 
-argsParser(args)
-    .then( res => console.log(res))
-    .catch(error => console.warn(error))
+createJSON(args)
+    .then(({ text, args }) => {
+        if (text) console.log(text);
+        return args;
+    })
+    .then((args) => argsParser(args))
+    .then((res) => console.log(res))
+    .catch((error) => console.log(error));
 
+// argsParser(args)
+//     .then((res) => console.log(res))
+//     .catch((error) => console.warn(error));
