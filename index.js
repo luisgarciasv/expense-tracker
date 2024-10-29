575c2e6758d4765bd3011c3b6a73d951d2ce2e4a
luisgarciasv <luisgarciasv3@gmail.com>
2024-10-29 16:14:20 -0600

    chore: update index.js for using new funntions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { argv } from 'node:process';
 import { argsParser } from './lib/args-parser.js';
 import { createJSON } from './lib/create-json.js';
+import { methodHandler } from './lib/method-handler.js';
 
 const args = argv.slice(2);
 
@@ -10,8 +11,12 @@ createJSON(args)
         return args;
     })
     .then((args) => argsParser(args))
+    .then((parsedArgs) => methodHandler(parsedArgs))
     .then((res) => console.log(res))
-    .catch((error) => console.log(error));
+    .catch((err) => {
+        console.warn(`${err.name}: ${err.message}`);
+        if (err.cause) console.warn(err.cause);
+    });
 
 // argsParser(args)
 //     .then((res) => console.log(res))
