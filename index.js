1471b14b5acc82dccac66ba0068243d8c31a5446
luisgarciasv <luisgarciasv3@gmail.com>
2024-10-31 22:59:31 -0600

    feat: add error handling for default js errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { argv } from 'node:process';
 import { argsParser } from './lib/args-parser.js';
 import { createJSON } from './lib/create-json.js';
 import { methodHandler } from './lib/method-handler.js';
+import { CustomError } from './lib/helpers.js';
 
 const args = argv.slice(2);
 
@@ -14,8 +15,12 @@ createJSON(args)
     .then((parsedArgs) => methodHandler(parsedArgs))
     .then((res) => console.log(res))
     .catch((err) => {
-        console.warn(`${err.name}: ${err.message}`);
-        if (err.cause) console.warn(err.cause);
+        if (err instanceof CustomError) {
+            console.warn(`${err.name}: ${err.message}`);
+            if (err.cause) console.warn(err.cause);
+        } else {
+            console.error(err)
+        }
     });
 
 // argsParser(args)
